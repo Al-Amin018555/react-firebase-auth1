ada04affbcf6abf36aeec894a0db73ac6329a680
Al-Amin018555 <alamin15-3799@diu.edu.bd>
2025-10-24 18:44:47 +0600

    learnt how to implement signout

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,19 +1,51 @@
-import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import { auth } from "../../firebase/firebase.init";
+import { useState } from "react";
 
 
 const Login = () => {
-
+    const [user, setUser] = useState(null);
     const provider = new GoogleAuthProvider();
     const handleGoogleSignIn = () => {
         console.log('google sign in clicked');
 
-        signInWithPopup(auth, provider).then(result => console.log(result)).catch(error => console.log(error))
+        signInWithPopup(auth, provider)
+            .then(result => {
+                console.log(result.user)
+                setUser(result.user)
+
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }
+
+    const handleSignOut = () => {
+        signOut(auth)
+            .then(() => {
+                console.log("Sign out successful");
+                setUser(null)
+            })
+            .catch((error) => {
+                console.log(error);
+            })
     }
     return (
         <div>
             <h2>Please Login</h2>
-            <button onClick={handleGoogleSignIn}>Sing in With Google</button>
+            {/* <button onClick={handleGoogleSignIn}>Sing in With Google</button>
+            <button onClick={handleSignOut} >Sign Out</button> */}
+            {
+                user ? <button onClick={handleSignOut} >Sign Out</button> : <button onClick={handleGoogleSignIn}>Sing in With Google</button>
+            }
+
+            {
+                user && <div>
+                    <p>{user.displayName}</p>
+                    <p>{user.email}</p>
+                    <img src={user.photoURL} alt="" />
+                </div>
+            }
         </div>
     );
 };
